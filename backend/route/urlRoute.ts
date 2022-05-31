1c708c12140da73468d36787e3cd07b166b222ef
Sammy <segzyfaj1@gmail.com>
2022-05-31 18:16:56 +0100

    A new Commit

--- backend/route/urlRoute.ts
+++ backend/route/urlRoute.ts
@@ -1,3 +1,16 @@
 const express = require('express')
 const validUrl = require('valid-url')
 const shortid = require('shortid')
+
+const router = express.Router()
+
+const Url = require("/model/urlModel")
+
+
+const baseUrl = ""
+
+router.post("shorten", (req, res) => {
+
+    const { longUrl } = req.body
+
+})
