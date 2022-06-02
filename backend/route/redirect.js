5dc70b82d4681469f5076e2c80d82e977bd8e61e
Sammy <segzyfaj1@gmail.com>
2022-06-02 13:47:53 +0100

    A new Commit

--- backend/route/redirect.js
+++ backend/route/redirect.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router()
+const urlModel = require("../model/urlModel")
 
-router.get("/:code", (req, res) => {
+router.get("/:code", async (req, res) => {
+
+    try {
+        const url = urlModel.findOne({
+            urlCode: req.params.code
+        })
+
+        if (url) {
+            res.redirect(url.longUrl)
+        } else {
+            res.status(404).json("Url Not Found!!")
+        }
+
+
+    } catch(err) {
+
+        console.log(err);
+        res.status(501).json("Server Error!!!")
+
+    }
 
 
 })
