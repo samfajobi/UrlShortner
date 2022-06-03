1fe841e497793db2125f8424861adb4a411ddde8
Sammy <segzyfaj1@gmail.com>
2022-06-03 23:57:36 +0100

    A new Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import urlPage from './components/urlPage';
 
 function App() {
   return (
+    <urlPage />
     
   );
 }
+
+
 
 export default App;
--- frontend/src/components/urlPage.js
+++ frontend/src/components/urlPage.js
@@ -1,19 +1,29 @@
 import styled from "styled-components"
+import useState from "react";
 
 
 const Container = styled.div`
   
 `
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+`
+
+const Input = styled.div`
+
+`
 
 
 
 const urlPage = () => {
+
+    const [ url, setUrl ] = useState("")
+
   return (
       <Container>
-          
-
+          <Wrapper>
+              <Input placeholder="Type in your URL....."/>
+          </Wrapper>
       </Container>
    
   )
