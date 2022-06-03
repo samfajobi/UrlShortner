48c78063674898eb31e2a76fe5c2c36876c4f0ef
Sammy <segzyfaj1@gmail.com>
2022-06-03 23:53:48 +0100

    A new Commit

---
+++ frontend/src/components/urlPage.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+
+const Container = styled.div`
+  
+`
+
+const Wrapper = styled.div``
+
+
+
+const urlPage = () => {
+  return (
+      <Container>
+          
+
+      </Container>
+   
+  )
+}
+
+export default urlPage
